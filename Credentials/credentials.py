d0ec41a253a8b3ed721f58b4bc38e205dfe94358
Prabhakar Joshi <70520718+prabhakarjoshi@users.noreply.github.com>
2022-07-04 17:41:06 +0530

    Removed credentials

--- Credentials/credentials.py
+++ Credentials/credentials.py
@@ -1,14 +1,6 @@
 #twitter credentials
-
-api_key = 'lEy67MdXngtKflMC38kJr1u6w'
-api_key_secret = 'FMTwcOA321V8cuMW8Hq1tkc3PhZnSBBKDKeQPlvVCF4gwfmrHG'
-access_token = '1500079761326219264-gmLQfR9XMh7fhWsRHu7W9zCplxOlpM'
-access_token_secret = 'C4La0DrHqJDsHW9QMo452JakPOHZK8KwuDDYum8V1qQSO'
-
-
-
-
-# api_key = 'V4lpiPa5bFuegeZ48n3Cao1zq'
-# api_key_secret = 'ksJwRF0QoJCsFslzgU4zN0utFBrwZu7VaxiFkraGty8t73LJFC'
-# access_token = '1540721724295356416-2QrTq7j8r4R6qSw6oi2bXuScc4RFgn'
-# access_token_secret = 'qoz4UWuerhr302QWnCXp9oM7GhPfo8GoVHNTJeA6zaNfn'
+# Add Credentials by creating an twitter developer account 
+api_key = ''
+api_key_secret = ''
+access_token = ''
+access_token_secret = ''
